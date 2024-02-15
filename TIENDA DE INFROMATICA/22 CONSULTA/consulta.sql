128e336fcad072894dfb9b130aa842dca279d12b
Marc Perarnau <151735878+MarcPerarnau@users.noreply.github.com>
2024-02-15 16:22:28 +0100

    Update consulta.sql

--- TIENDA DE INFROMATICA/22 CONSULTA/consulta.sql
+++ TIENDA DE INFROMATICA/22 CONSULTA/consulta.sql
@@ -1,3 +1,9 @@
+/*Con Alias*/
 SELECT p.nombre AS nombre_producto, p.precio, f.nombre AS nombre_fabricante
 FROM producto p
 JOIN fabricante f ON p.id_fabricante = f.id;
+
+/*Sin Alias*/
+SELECT producto.nombre AS nombre_producto, producto.precio, fabricante.nombre AS nombre_fabricante
+FROM producto
+JOIN fabricante ON producto.id_fabricante = fabricante.id;
